53ab9f01f569bfca26ac0a964867346dbda77866
jakezhou <jakezhou>
2023-05-04 18:07:55 -0400

    start of project, wrote major steps to do

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,25 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        /**
+         * TODO:
+         * read messages
+         *
+         * use map to link symbol to execution volume by shares
+         *
+         * two types to focus on: Orders and Trade Messages
+         * store Add Order to map, wait for order cancel or order executed to count
+         * read trade messages for volume of hidden order
+         *
+         * ignore Trade Break, Symbol Clear, Auction, Status, Retail, and long messages (‘B’, ‘s’, ‘r’, ‘d’, ‘H’, ‘I’, ‘J’, ‘R’)
+         *
+         * Iterate over map to get the top 10 values
+         */
+
+
+
+
+        System.out.println("Hello world!");
+    }
+}
